85f665de31ce15bc20d0cad8febec8bfe633d66e
Mikhail Kardash <mikhail.kardash@hpe.com>
2024-10-17 15:36:53 -0700

    iter on tests

--- harness/tests/experiment/pytorch/test_deepspeed_trial.py
+++ harness/tests/experiment/pytorch/test_deepspeed_trial.py
@@ -1,9 +1,13 @@
+import argparse
+import os
 import pathlib
 import pytest
 import sys
 import typing
 
+import determined as det
 from determined import pytorch
+
 from tests.experiment import deepspeed_utils, utils  # noqa: I100
 from tests.experiment.fixtures import deepspeed_linear_model
 
@@ -23,63 +27,35 @@ class TestDeepSpeedTrial:
             "disable_dataset_reproducibility_checks": False,
         }
     def test_deepspeed_linear(self, tmp_path: pathlib.Path) -> None:
-        """Assert that the training loss and validation error decrease monotonically."""
-        tensorboard_path = tmp_path.joinpath("tensorboard")
-        trial, trial_controller = deepspeed_utils.create_trial_and_trial_controller(
-            trial_class=deepspeed_linear_model.LinearDeepSpeedTrial,
-            hparams=self.hparams,
-            trial_seed=self.trial_seed,
-            tensorboard_path=tensorboard_path,
-        )
+        def linear_trial_loss_validation(tmp_path: pathlib.Path):
+            """Assert that the training loss and validation error decrease monotonically."""
+            tensorboard_path = tmp_path.joinpath("tensorboard")
+            trial, trial_controller = deepspeed_utils.create_trial_and_trial_controller(
+                trial_class=deepspeed_linear_model.LinearDeepSpeedTrial,
+                hparams=self.hparams,
+                trial_seed=self.trial_seed,
+                tensorboard_path=tensorboard_path,
+            )
 
-        train_steps, metrics = trial_controller._train_with_boundaries(
-            training_enumerator=enumerate(trial_controller.training_iterator),
-            train_boundaries=[
-                pytorch._TrainBoundary(
-                    step_type=pytorch._TrainBoundaryType.TRAIN, unit=pytorch.Batch(100)
-                )
-            ],
-        )
+            train_steps, metrics = trial_controller._train_with_boundaries(
+                training_enumerator=enumerate(trial_controller.training_iterator),
+                train_boundaries=[
+                    pytorch._TrainBoundary(
+                        step_type=pytorch.TrainBoundaryType.TRAIN, unit=pytorch.Batch(100)
+                    )
+                ],
+            )
 
-        assert len(train_steps) == 1, "unexpected train step count"
-        assert train_steps[0].limit_reached, "train step did not reach expected limit"
-        assert len(metrics) == 100, "metrics length did not match input"
+            assert len(train_steps) == 1, "unexpected train step count"
+            assert train_steps[0].limit_reached, "train step did not reach expected limit"
+            assert len(metrics) == 100, "metrics length did not match input"
 
-        for older, newer in zip(metrics, metrics[1:]):
-            assert newer["loss"] <= older["loss"]
-
-    def test_training_metrics(self, tmp_path: pathlib.Path) -> None:
-        tensorboard_path = tmp_path.joinpath("tensorboard")
-
-        trial, trial_controller = deepspeed_utils.create_trial_and_trial_controller(
-            trial_class=deepspeed_linear_model.LinearDeepSpeedTrial,
-            hparams=self.hparams,
-            trial_seed=self.trial_seed,
-            tensorboard_path=tensorboard_path,
-        )
-
-        train_steps, metrics = trial_controller._train_with_boundaries(
-            training_enumerator=enumerate(trial_controller.training_iterator),
-            train_boundaries=[
-                pytorch._TrainBoundary(
-                    step_type=pytorch._TrainBoundaryType.TRAIN, unit=pytorch.Batch(100)
-                )
-            ],
-        )
-
-        assert len(train_steps) == 1, "unexpected train step count"
-        assert train_steps[0].limit_reached, "train step did not reach expected limit"
-        assert len(metrics) == 100, "metrics length did not match input"
-        for metric in metrics:
-            assert "mse" in metric
+            for older, newer in zip(metrics, metrics[1:]):
+                assert newer["loss"] <= older["loss"]
 
     def test_checkpointing_and_restoring(self, tmp_path: pathlib.Path) -> None:
-        updated_hparams = {
-            "lr_scheduler_step_mode": pytorch.LRScheduler.StepMode.STEP_EVERY_BATCH.value,
-            **self.hparams,
-        }
         self.checkpoint_and_check_metrics(
-            deepspeed_linear_model.LinearDeepSpeedTrial, updated_hparams, tmp_path, (100, 100)
+            deepspeed_linear_model.LinearDeepSpeedTrial, tmp_path, (100, 100)
         )
     def checkpoint_and_check_metrics(
         self,
